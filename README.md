1eaa4632411b1e32680cd1ffe8bc775c36be11be
L <chinaume@163.com>
2019-07-15 21:29:32 +0800

    license

--- README.md
+++ README.md
@@ -1,29 +1,12 @@
-# cv
-
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn run serve
-```
-
-### Compiles and minifies for production
-```
-yarn run build
-```
-
-### Run your tests
-```
-yarn run test
-```
-
-### Lints and fixes files
-```
-yarn run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+ <div>
+     <a rel="license"  href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+        <img alt="Creative Commons License" style="border-width:0"
+             src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png"/>
+      </a>
+      <br/>
+      License By
+      <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+        Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License
+      </a>
+      .
+</div>
--- src/components/cFooter.vue
+++ src/components/cFooter.vue
@@ -1,9 +1,21 @@
 <template>
   <footer>
-    <p>Copyright &copy;{{year}} -
+    <span>Copyright &copy;{{year}} -
       <el-link href="https://github.com/anmokoto" :underline="false" target="_blank">Mr.Xu</el-link>
-      .All Rights Reserved.
-    </p>
+      . All Rights Reserved .
+    </span>
+    <div>
+      <el-link rel="license" :underline="false" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"
+               target="_blank">
+        <img alt="Creative Commons License" style="border-width:0"
+             src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png"/></el-link>
+      <br/>License By
+      <el-link rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">
+        Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License
+      </el-link>
+      .
+    </div>
+    <!--    <el-link rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></el-link><br />This work is licensed under a <el-link rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</el-link>.-->
   </footer>
 </template>
 
@@ -27,6 +39,7 @@ export default {
     color #606266
     outline 0
     padding 0
+
     *
       vertical-align text-bottom !important
 
